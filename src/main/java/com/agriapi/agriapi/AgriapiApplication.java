54c289822175c5afea465948d22c1d10690fa66c
Marcos <magma_ruby145@hotmail.com>
2023-08-24 22:40:26 -0700

    test neobimianio

---
+++ src/main/java/com/agriapi/agriapi/AgriapiApplication.java
@@ -0,0 +1,13 @@
+package com.agriapi.agriapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agriapi/agriapi/AgriapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriapi.agriapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
